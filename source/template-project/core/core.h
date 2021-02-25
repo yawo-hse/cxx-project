df6d93572e1f4a6c5746262fa2e4e9a8e12cdb3d
Mitya Selivanov <0x7fffff@guattari.ru>
2021-02-25 16:24:03 +0300

    Remove pragma once.

--- source/template-project/core/core.h
+++ source/template-project/core/core.h
@@ -1,5 +1,8 @@
-#pragma once
+#ifndef template_project_core_core_h
+#define template_project_core_core_h
 
 namespace yetanotherproject {
   [[nodiscard]] auto get_value() noexcept -> int;
 }
+
+#endif
