f99b23f344634e31a61f0e96f2a2263efb8c0f3d
Mitya Selivanov <0x7fffff@guattari.ru>
2021-03-21 11:46:05 +0300

    Change comments.

--- source/cxx-project/core/core.h
+++ source/cxx-project/core/core.h
@@ -3,11 +3,6 @@
  *  Copyright (c) 2021 Mitya Selivanov
  *
  *  This file is part of the cxx-project.
- *
- *  Laplace is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty
- *  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
- *  the MIT License for more details.
  */
 
 #ifndef cxx_project_core_core_h
